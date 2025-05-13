f6a9e4aff848a281506260fa3350c5ddb40b93aa
kopy-kat <kopp.konrad@gmail.com>
2025-05-13 11:43:33 +0100

    feat: add testnets

--- ponder.config.ts
+++ ponder.config.ts
@@ -7,6 +7,7 @@ import { originAbi } from "./abis/originAbi";
 export default createConfig({
   ordering: "multichain",
   networks: {
+    // Mainnets
     mainnet: {
       chainId: 1,
       transport: http(process.env.PONDER_RPC_URL_1),
@@ -23,6 +24,23 @@ export default createConfig({
       chainId: 42161,
       transport: http(process.env.PONDER_RPC_URL_42161),
     },
+    // Testnets
+    sepolia: {
+      chainId: 11155111,
+      transport: http(process.env.PONDER_RPC_URL_11155111),
+    },
+    baseSepolia: {
+      chainId: 84532,
+      transport: http(process.env.PONDER_RPC_URL_84532),
+    },
+    optimismSepolia: {
+      chainId: 11155420,
+      transport: http(process.env.PONDER_RPC_URL_11155420),
+    },
+    arbitrumSepolia: {
+      chainId: 421614,
+      transport: http(process.env.PONDER_RPC_URL_421614),
+    },
   },
   contracts: {
     rsSpokePool: {
@@ -33,6 +51,14 @@ export default createConfig({
         base: { address: "0x000000000060f6e853447881951574CDd0663530" },
         optimism: { address: "0x000000000060f6e853447881951574CDd0663530" },
         arbitrum: { address: "0x000000000060f6e853447881951574CDd0663530" },
+        sepolia: { address: "0x000000000060f6e853447881951574CDd0663530" },
+        baseSepolia: { address: "0x000000000060f6e853447881951574CDd0663530" },
+        optimismSepolia: {
+          address: "0x000000000060f6e853447881951574CDd0663530",
+        },
+        arbitrumSepolia: {
+          address: "0x000000000060f6e853447881951574CDd0663530",
+        },
       },
     },
     originModule: {
@@ -43,6 +69,14 @@ export default createConfig({
         base: { address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8" },
         optimism: { address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8" },
         arbitrum: { address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8" },
+        sepolia: { address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8" },
+        baseSepolia: { address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8" },
+        optimismSepolia: {
+          address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8",
+        },
+        arbitrumSepolia: {
+          address: "0x0000000000AFc904aE9860D9c4B96D7c529c58b8",
+        },
       },
     },
     sameChainModule: {
@@ -53,6 +87,14 @@ export default createConfig({
         base: { address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04" },
         optimism: { address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04" },
         arbitrum: { address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04" },
+        sepolia: { address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04" },
+        baseSepolia: { address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04" },
+        optimismSepolia: {
+          address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04",
+        },
+        arbitrumSepolia: {
+          address: "0x000000000043ff16d5776c7F0f65Ec485C17Ca04",
+        },
       },
     },
   },
